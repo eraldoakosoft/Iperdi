65f33c49d7c0f5087a5c6ca11103a98ed20f2292
Eraldo <eraldo450518@outlook.com>
2021-02-15 23:59:27 -0300

    Criado a tela de cadastro

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,17 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
-import Login from './views/Login'
+//import Login from './views/Login'
 import Cadastro from './views/Cadastro'
 
 const Stack = createStackNavigator()
 
 function Routes(){
     return(
-        <NavigationContainer  >
-            <Stack.Navigator>
-                <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
+        <NavigationContainer >
+            <Stack.Navigator >
                 <Stack.Screen name='Cadastro' component={Cadastro} />
+                {/*<Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />*/}
             </Stack.Navigator>
         </NavigationContainer>
     )
--- src/views/Cadastro.js
+++ src/views/Cadastro.js
@@ -1,10 +1,90 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import { View, KeyboardAvoidingView, Image, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
 
-export default function Cadastro(){
-    return(
-        <View>
-            <Text>Cadastro!</Text>
-        </View>
-    )
+export default function App(){
+  return(
+    <KeyboardAvoidingView style={styles.background} >
+      <View style={styles.containerLogo} >
+        <Image source={require('../assets/logo-branco.png')} />
+      </View>
+      <View style={styles.container} >
+        <TextInput
+          style={styles.input}
+          placeholder="Nome"
+          autoCorrect={false}
+          onChangeText={ () => {} }
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          autoCorrect={false}
+          onChangeText={ () => {} }
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          autoCorrect={false}
+          onChangeText={ () => {} }
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Confirmar Senha"
+          autoCorrect={false}
+          onChangeText={ () => {} }
+        />
+
+        <TouchableOpacity style={styles.btnSubmit} >
+          <Text style={styles.submitText} >Criar</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  )
 }
+
+const styles = StyleSheet.create({
+  background:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#595959'
+  },
+  containerLogo:{
+    flexGrow: 1,
+    justifyContent: 'flex-end',
+    marginTop: 15,
+  },
+  container:{
+    flex: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    paddingBottom: 50
+  },
+  input:{
+    backgroundColor: '#FFF',
+    width: '90%',
+    marginBottom: 15,
+    color: '#222',
+    fontSize: 17,
+    borderRadius: 7,
+    padding: 10,
+  },
+  btnSubmit:{
+    backgroundColor: '#2F2F2F',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+  },
+  submitText:{
+    color: '#FFF',
+    fontSize: 18
+  },
+  btnRegister:{
+    marginTop: 10
+  },
+  registerText:{
+    color: '#FFF'
+  }
+})
