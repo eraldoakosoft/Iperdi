406d897279253fb08f07b37473f072f5592cf7b3
Eraldo <eraldo450518@outlook.com>
2021-02-17 23:32:41 -0300

    Criado a tela Perfil

--- src/views/Perfil.js
+++ src/views/Perfil.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Text, View } from 'react-native'
-import { ListItem, Icon } from 'react-native-elements'
+import { ScrollView } from 'react-native-gesture-handler'
+import { ListItem } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 import HeaderPerfil from '../components/HeaderPerfil'
 
@@ -8,33 +10,69 @@ export default function Perfil() {
 
     const list = [
         {
-            title: 'Appointments',
-            icon: 'av-timer'
+            title: 'Notificações',
+            icon: 'notifications',
+            subtitle: 'Minha central de notificações'
         },
         {
-            title: 'Trips',
-            icon: 'flight-takeoff'
+            title: 'Endereço',
+            icon: 'location',
+            subtitle: 'Meu endereço'
+        },
+        {
+            title: 'Meus dados',
+            icon: 'clipboard',
+            subtitle: 'Minhas informações da conta'
+        },
+        {
+            title: 'Carteira',
+            icon: 'wallet',
+            subtitle: 'Meus documentos cadastrados'
+        },
+        {
+            title: 'Notificações',
+            icon: 'notifications',
+            subtitle: 'Minha central de notificações'
+        },
+        {
+            title: 'Endereço',
+            icon: 'location',
+            subtitle: 'Meu endereço'
+        },
+        {
+            title: 'Meus dados',
+            icon: 'clipboard',
+            subtitle: 'Minhas informações da conta'
+        },
+        {
+            title: 'Carteira',
+            icon: 'wallet',
+            subtitle: 'Meus documentos cadastrados'
         }
     ]
 
     return (
-        <View>
+        <View style={{flex: 1, paddingEnd: 15}}>
             <View>
-                <HeaderPerfil/>
+                <HeaderPerfil />
             </View>
+            <ScrollView showsHorizontalScrollIndicator={false} >
+                <View>
+                    {
+                        list.map((item, i) => (
+                            <ListItem key={i} bottomDivider>
+                                <Icon name={item.icon} size={35} color="#2F2F2F" />
+                                <ListItem.Content>
+                                    <ListItem.Title>{item.title}</ListItem.Title>
+                                    <ListItem.Subtitle>{item.subtitle}</ListItem.Subtitle>
+                                </ListItem.Content>
+                                <ListItem.Chevron color="#000" size={30} />
+                            </ListItem>
+                        ))
+                    }
+                </View>
+            </ScrollView>
 
-
-            {
-                list.map((item, i) => (
-                    <ListItem key={i} bottomDivider>
-                        <Icon name={item.icon} />
-                        <ListItem.Content>
-                            <ListItem.Title>{item.title}</ListItem.Title>
-                        </ListItem.Content>
-                        <ListItem.Chevron />
-                    </ListItem>
-                ))
-            }
         </View>
     )
 }
